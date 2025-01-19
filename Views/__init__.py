991d37c02954db9eadde8284bd16bfbcd8a42ffe
ShernOC <sherlynea1994@hotmail.com>
2025-01-20 01:24:44 +0300

    added _init_ folder

---
+++ Views/__init__.py
@@ -0,0 +1,5 @@
+#import all the functions 
+from .user import *
+from .staff import *
+from .feed import *
+
--- app.py
+++ app.py
@@ -7,3 +7,22 @@ app = Flask(__name__)
 
 # #create a migration. config parameters
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///customer.db'
+
+# #initialize the router 
+migrate = Migrate(app,db)
+db.init_app(app)
+
+#import all the functions in views 
+from Views import * 
+
+#register the blueprints 
+app.register_blueprint(user_bp)
+app.register_blueprint(feed_bp)
+app.register_blueprint(staff_bp)
+
+@app.route('/')
+def index(): 
+    return jsonify("<h1> Customer Service Management System </h1>")
+
+
+
